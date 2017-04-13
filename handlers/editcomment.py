72bf5f606f9cb7696e394d8a682ceab8ca2139fa
perry kong <apk29@hotmail.com>
2017-04-12 17:23:15 -0700

    after review, 8

--- handlers/editcomment.py
+++ handlers/editcomment.py
@@ -46,13 +46,6 @@ class EditComment(BlogHandler):
                               "?error=You don't have access to edit this " +
                               "comment.")
 
-            if c.user_id == self.user.key().id():
-                self.render("editcomment.html", comment=c.comment)
-            else: 
-                self.redirect("/blog/" + post_id +
-                              "?error=You don't have access to edit this " +
-                              "comment.")
-
             c.comment = comment
             c.put()
             self.redirect('/blog/%s' % post_id)
